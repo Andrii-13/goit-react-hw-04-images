941693c4d4beab1e5954212fc29f9c36520b596a
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-10-17 03:50:14 +0300

    finish 3

--- src/js/respFromBack.js
+++ src/js/respFromBack.js
@@ -3,7 +3,7 @@ import { getFetch } from './api';
 export const respFromBack = async (name, page, imgOnPage) => {
   let responce = [];
   for (let i = 1; i <= page; i += 1) {
-    const {hits, totalHits} = await getFetch(name, i, imgOnPage)
+    const {hits} = await getFetch(name, i, imgOnPage)
     responce = [...responce, ...hits]
   }
   return responce;
